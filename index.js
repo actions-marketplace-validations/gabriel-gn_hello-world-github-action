e1283176f82e51ae09d61afebe7ca6365936464e
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-04-25 15:53:56 -0300

    outputs

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const core = require('@actions/core');
 const {promises: fs} = require('fs')
 
-const readPackageJson = async () => {
-    const filepath = core.getInput('filepath')
-    core.info(`Reading file ${filepath}`);
-    const content = await fs.readFile(filepath, 'utf8')
-    core.info(content);
-    core.setOutput('content', content)
-}
-
 // most @actions toolkit packages have async methods
 async function run() {
     try {
         const filepath = core.getInput('filepath')
         core.info(`Reading file ${filepath}`);
-        const content = await fs.readFile(filepath, 'utf8')
-        core.info(content);
+        let content = await fs.readFile(filepath, 'utf8')
+        content = JSON.parse(content);
+        let npmVersion = content.version;
         core.setOutput('content', content)
-        // core.setOutput('time', new Date().toTimeString());
+        core.setOutput('packageName', content.name)
+        core.setOutput('version', content.version)
+        core.setOutput('npmVersion', content.version)
+        core.setOutput('changed', content.version != npmVersion)
+        core.info(`npm version: ${content.version}`);
+        core.info(`file version: ${content.version}`);
+        core.info(`is package version in npm versions?: ${content.version != npmVersion}`);
     } catch (error) {
         core.setFailed(error.message);
     }
